711d86886279c2588951237fe799a3447903a6fc
Francisco Árciga Nolasco <Francisco@Kribrick.local>
2021-04-24 10:51:53 -0500

    Edit some things in Empresa

--- routes/empresa.route.js
+++ routes/empresa.route.js
@@ -6,7 +6,7 @@ router.get("/", empresaController.select_empresas);
 router.get("/:id", empresaController.select_empresa);
 router.post("/login", empresaController.login);
 router.post("/insert", empresaController.insert_empresa);
-router.put("/update", empresaController.update_usuario);
+router.put("/update", empresaController.update_empresa);
 router.put("/resetPwd", empresaController.update_Pwd);
 router.delete("/delete/:id", empresaController.delete_usuario);
 
